59d7c3458350b6a236f222611c8ce7d65881eb78
geekswaroop <kkrishnaswaroop99@gmail.com>
2019-12-10 21:44:00 +0530

    Added NLP Template

---
+++ preprocessing_template.py
@@ -0,0 +1,108 @@
+ # Your One-Stop guide for messy Text Preprocessing!
+ # Load Word Vectors, Transform strings to integers and get 
+ # fresh trainable data. All in ONE GO!
+
+from __future__ import print_function, division
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Input, GlobalMaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.models import Model
+from sklearn.metrics import roc_auc_score
+
+
+# Download the data:
+# Download the word vectors:
+# http://nlp.stanford.edu/data/glove.6B.zip
+
+
+# some configuration
+MAX_SEQUENCE_LENGTH = 100
+MAX_VOCAB_SIZE = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+BATCH_SIZE = 128
+EPOCHS = 10
+
+
+
+# STEP1: load in pre-trained word vectors
+print('Loading word vectors...')
+word2vec = {}
+with open(os.path.join('../large_files/glove.6B/glove.6B.%sd.txt' % EMBEDDING_DIM)) as f:
+  # is just a space-separated text file in the format:
+  # word vec[0] vec[1] vec[2] ...
+  for line in f:
+    values = line.split()
+    word = values[0]
+    vec = np.asarray(values[1:], dtype='float32')
+    word2vec[word] = vec
+print('Found %s word vectors.' % len(word2vec))
+
+
+
+#STEP2: prepare text samples and their labels
+print('Loading in comments...')
+
+train = pd.read_csv("..") #Insert your file path where the text csv is stored.
+sentences = train["comment_text"].fillna("DUMMY_VALUE").values
+possible_labels = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+targets = train[possible_labels].values
+
+
+
+#STEP3: convert the sentences (strings) into integers
+tokenizer = Tokenizer(num_words=MAX_VOCAB_SIZE)
+tokenizer.fit_on_texts(sentences)
+sequences = tokenizer.texts_to_sequences(sentences)
+# print("sequences:", sequences); exit()
+
+
+print("max sequence length:", max(len(s) for s in sequences))
+print("min sequence length:", min(len(s) for s in sequences))
+s = sorted(len(s) for s in sequences)
+print("median sequence length:", s[len(s) // 2])
+
+
+#STEP4: get word -> integer mapping
+word2idx = tokenizer.word_index
+print('Found %s unique tokens.' % len(word2idx))
+
+
+#STEP5: pad sequences so that we get a N x T matrix
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+print('Shape of data tensor:', data.shape)
+
+
+
+#STEP6: prepare embedding matrix
+print('Filling pre-trained embeddings...')
+num_words = min(MAX_VOCAB_SIZE, len(word2idx) + 1)
+embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+for word, i in word2idx.items():
+  if i < MAX_VOCAB_SIZE:
+    embedding_vector = word2vec.get(word)
+    if embedding_vector is not None:
+      # words not found in embedding index will be all zeros.
+      embedding_matrix[i] = embedding_vector
+
+
+#STEP7:
+# load pre-trained word embeddings into an Embedding layer
+# note that we set trainable = False so as to keep the embeddings fixed
+embedding_layer = Embedding(
+  num_words,
+  EMBEDDING_DIM,
+  weights=[embedding_matrix],
+  input_length=MAX_SEQUENCE_LENGTH,
+  trainable=False
+)
